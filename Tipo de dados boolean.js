611602f91be5b1eff053bed4fcb2d542c32eb736
walterdyna <alexdyna@gmail.com>
2023-04-09 23:35:54 -0300

    Tipo de valores Boolean

---
+++ Tipo de dados boolean.js
@@ -0,0 +1,20 @@
+//Bolean
+
+// Valore Boolenos = 0 e 1 mais no JS falamos de Verdadeiro ou falso ou TRUE e FALSE
+
+
+let myNumberOne = 10
+let myNumberTwo = 20
+
+console.log (myNumberOne = myNumberTwo) // EX: desta forma estamos falando que o NUMERO-1 E IGUAL AO NUMERO-2 ou recebe
+
+let myNumber1 = 10
+let myNumber2 = 20
+
+console.log (myNumber1 == myNumber2) // Ex: desta forma estou fazendo uma comparação... verificando se o numero 1 e igual ao 2
+
+let myNumberOne1 = 20
+let myNumberTwo2 = 20
+
+console.log (myNumberOne1 == myNumberTwo2) // Ex: Desta forma dasse a entender que == dois sinais de igual junto estamos comparando uma informaçao.
+
